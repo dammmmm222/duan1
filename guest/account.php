d88925b1849c797f847c69167af6b5f717181d49
phithph22381 <103241880+dammmmm222@users.noreply.github.com>
2022-12-05 03:29:07 +0700

    Update account.php

--- guest/account.php
+++ guest/account.php
@@ -1,45 +1,41 @@
-<?php
-function loadall_taikhoan()
-{
-    $sql = "select * from user order by id desc";
-    $listtaikhoan = pdo_query($sql);
-    return $listtaikhoan;
-}
-function getaddress($id)
-{
-    $sql = "SELECT `full_name`, `phone`, `address` FROM `user` WHERE id =". $id;
-    $address = pdo_query_one($sql);
-    return $address;
-}
-function search_id()
-{
-    $sql = "select id from user order by id desc LIMIT 0,1";
-    $listtaikhoan = pdo_query_one($sql);
-    $iduser = $listtaikhoan['id'];
-    return $iduser;
-}
-function insert_taikhoan($user, $pass, $email)
-{
-    $sql = "insert into taikhoan(user,pass,email) values('$user','$pass','$email')";
+if ($hinh != "") {
+        // Update thêm cả hình ảnh
+        $sql = "UPDATE khach_hang SET ho = '$ho', ten = '$ten', hinh = '$hinh', email = '$email' WHERE ma_kh = '$ma_kh'";
+    } else {
+        // Ngược lại thì không update hình ảnh
+        $sql = "UPDATE khach_hang SET ho = '$ho', ten = '$ten', email = '$email' WHERE ma_kh = '$ma_kh'";
+    }
     pdo_execute($sql);
 }
 
-function check_email($email)
+// Cập nhật tài khoản phía admin
+function cap_nhat_tai_khoan_admin($id, $user_name, $full_name, $email, $phone, $address, $password, $role)
 {
-    $sql = "select * from taikhoan where email='" . $email . "'";
-    $kq = pdo_query_one($sql);
-    return $kq;
+    $sql = "UPDATE `user` SET `user_name`='$user_name',`full_name`='$full_name',`email`='$email',`phone`='$phone',`address`='$address',`password`='$password',`role`='$role' WHERE `id` = '$id'";
+    pdo_execute($sql);
 }
 
-function check_user($user, $pass)
+// Xóa tài khoản theo mã khách hàng
+function xoa_tai_khoan($id)
 {
-    $sql = "select * from user where user_name='" . $user . "' AND password='" . $pass . "'";
-    $kq = pdo_query_one($sql);
-    return $kq;
+    $sql = "DELETE FROM user WHERE id = '$id'";
+    pdo_execute($sql);
 }
 
-function update_taikhoan($id, $user, $pass, $email, $address, $tel)
+// Kiểm tra email
+function ktra_email($email)
 {
-    $sql = "update taikhoan set user='" . $user . "', pass='" . $pass . "',email='" . $email . "',address='" . $address . "',tel='" . $tel . "'where id=" . $id;
+    // Truy vấn đến tài khoản mà email trùng với tham số email truyền vào
+    $sql = "SELECT * FROM khach_hang WHERE email = '$email'";
+    $tai_khoan_email = pdo_query_one($sql);
+    return $tai_khoan_email;
+}
+
+// Kiểm tài khoản có tồn tại trong database ko
+
+// Cập nhật mật khẩu theo mã khách hàng
+function doi_mat_khau_tai_khoan($ma_kh, $mat_khau_moi)
+{
+    $sql = "UPDATE khach_hang SET mat_khau = '$mat_khau_moi' WHERE ma_kh = '$ma_kh'";
     pdo_execute($sql);
 }
